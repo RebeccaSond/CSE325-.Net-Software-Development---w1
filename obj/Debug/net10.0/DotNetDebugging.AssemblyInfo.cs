c5525a4179142d16b37d9478ec3efab96eafc771
LokiObahoshe <sondereggerbecca@gmail.com>
2026-03-04 17:44:21 -0600

    Error#1

--- obj/Debug/net10.0/DotNetDebugging.AssemblyInfo.cs
+++ obj/Debug/net10.0/DotNetDebugging.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75adf6f2408f7b04f31c25a821258424d036ca01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4358121a760fdc9f5799f1b674ee57000e79649b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
