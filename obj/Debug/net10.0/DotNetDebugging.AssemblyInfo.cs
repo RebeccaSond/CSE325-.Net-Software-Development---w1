203e1822e1c3d8ee67c092da9e2d2c94af06f63a
LokiObahoshe <sondereggerbecca@gmail.com>
2026-03-04 18:22:04 -0600

    Added stuff

--- obj/Debug/net10.0/DotNetDebugging.AssemblyInfo.cs
+++ obj/Debug/net10.0/DotNetDebugging.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5525a4179142d16b37d9478ec3efab96eafc771")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1fe63c49542066e297086fe8571aeef6fd65e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
